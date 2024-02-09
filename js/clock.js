3e43bb8ba4769a6281e09cde17fa6dd3a73fc9ec
sid773 <siddu10.klm@gmail.com>
2024-02-09 12:49:04 +0530

    big changes

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2024-01-05 12:00", "Asia/Kolkata");
+  let targetDate = moment.tz("2024-02-18 05:00", "Asia/Kolkata");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
